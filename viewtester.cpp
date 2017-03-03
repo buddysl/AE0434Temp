78b19cccc40dfb104565102c7b24d4504159e998
buddysl <buddysl@yahoo.ca>
2017-03-03 16:34:57 -0500

    Updated viewtest.cpp to clean up command-line program. Tested

--- viewtester.cpp
+++ viewtester.cpp
@@ -13,6 +13,7 @@ int main(int argc, char **argv) {
 
 	CImageProcessingEngine ipengine;
 	Mat image;
+	bool alwaysShowImage = true;
 
 	while (1 == 1) {
 		printf("Enter input: ");
@@ -20,7 +21,7 @@ int main(int argc, char **argv) {
 		if (strcmp(s, "quit") == 0) {
 			break;
 		}
-		else if (strcmp(s, "display") == 0) {
+		else if (strcmp(s, "load") == 0) {
 			printf("Enter filename: ");
 			scanf("%s", &s);
 
@@ -32,23 +33,42 @@ int main(int argc, char **argv) {
 				printf("No image data (warning: OpenCV recognize files by extensions)\n");
 				break;
 			}
-			ipengine.DISPLAY_IMAGE(image,"Input");
+			if(alwaysShowImage) ipengine.DISPLAY_IMAGE(image, "Initial image");
+		}
+		else if (strcmp(s, "show_image") == 0) {
+			if (!image.data)
+				printf("No image loaded.  Use load command.\n");
+			else
+				ipengine.DISPLAY_IMAGE(image,"Current image");
+		}
+		else if (strcmp(s, "save") == 0) {
+			printf("Enter filename: ");
+			scanf("%s", &s);
+
+			cv::String filename;
+			ipengine.SAVE_PIXELS(image, "output.png"); //bug
+			printf("Saved.\n");
 		}
 		else if (strcmp(s, "find_region") == 0) {
-			printf("Enter filename: ");
-			scanf("%s", &s);
 
-			cv::String filename;
-			filename = s;
-			image = imread(filename, 1);
-			if (!image.data) {
-				printf("No image data (warning: OpenCV recognize files by extensions)\n");
-				break;
-			}
 			Mat find_region_image;
 			find_region_image = image.clone();
 			ipengine.FIND_REGION(image, find_region_image, 1, 1);
-			ipengine.DISPLAY_IMAGE(find_region_image, "FIND REGION RESULTS");
+			image = find_region_image;
+			if (alwaysShowImage) ipengine.DISPLAY_IMAGE(image, "Find_region Results");
+		}
+		else if (strcmp(s, "find_perimeter") == 0) {
+
+		}
+		else if (strcmp(s, "toggle_display_mode") == 0){
+			alwaysShowImage = !alwaysShowImage;
+			if (alwaysShowImage)
+				printf("Display mode: show image.\n");
+			else
+				printf("Display mode: do not show image.\n");
+		}
+		else {
+			printf("Invalid command. Try again.\n");
 		}
 	}
 
